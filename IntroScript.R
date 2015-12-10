d6403a2d2d0742dc807331a186ab7d4994885c56
Manu <mlf1g15@soton.ac.uk>
2015-12-10 19:58:34 +0000

    First try to query with rmongodb package

--- IntroScript.R
+++ IntroScript.R
@@ -9,7 +9,16 @@ mongo <- mongo.create()
 #Connect to mongodb file previously created named "mongo"
 mongo.is.connected(mongo)
 
-#Get db from mongo (created there)
+#Get db from mongo (if created there)
 mongo.get.databases(mongo)
 
-#
+
+
+
+
+
+
+mongDB <- mongo.bson.from.df(datab)
+
+#Create database in mongo
+mongo.insert.batch(mongo, "manu.tweets", mongDB)
